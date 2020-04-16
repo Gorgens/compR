2eb13f08f5a8da9d25d672c50b72396f539c9ae6
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2020-04-16 10:46:28 -0300

    Small fixes.

--- amostragem/preparacao.R
+++ amostragem/preparacao.R
@@ -1,11 +1,8 @@
-# Manipulação de dataframe
+# Manipulacao de dataframe
 # Eric Bastos Gorgens
 # criado em: 14/04/2020
 
-require(magrittr)
-require(dplyr)
-
-df = read.csv('dadosinvt.csv')
+df = read.csv('./youtube a4/inventario.csv')
 
 names(df)
 head(df)
@@ -19,28 +16,33 @@ summary(df)
 str(df)
 df$dataplantio = as.Date(df$dataplantio, format = '%d/%m/%Y')
 df$datamedicao = as.Date(df$datamedicao, format = '%d/%m/%Y')
-
-df$dataplantio %<>% as.Date(format = '%d/%m/%Y')
-df$datamedicao %<>% as.Date(format = '%d/%m/%Y')
-
-#write.csv(df, 'df_processed.csv')
+str(df)
 
 # Obter a altura dominante por parcela
 
-dfHd = df %>%
-	filter(obs == "D") %>%
-	group_by(talhao, parcela) %>%
-	summarize(hd = mean(ht)) %>%
-	left_join(df, ., by=c('talhao', 'parcela'))
+dfHd = df[df$obs == "D",]
 
-# Estimar altura das árvores que não foram medidas
+unique(dfHd$talhao)
+unique(dfHd$parcela)
+
+dfHd$chave = paste(dfHd$talhao, ".", dfHd$parcela, sep = "")
+hdMean = data.frame(chave = names(tapply(dfHd$ht, dfHd$chave, mean)),
+                       Hd = tapply(dfHd$ht, dfHd$chave, mean))
+
+df$chave = paste(df$talhao, ".", df$parcela, sep = "")
+df2 = merge(df, hdMean, by = 'chave')
+
+# Estimar altura das arvores que nao foram medidas
 
 dfCoef = data.frame(talhao = c(2, 3, 4),
 				   b0 = c(1.441593, 0.682470, 1.278235), 
 				   b1 = c(-5.593725, -4.306083, -5.21543),
 				   b2 = c(0.661824, 0.872517, 0.7234712))
 
-dfHdCoef = dfCoef %>% left_join(dfHd,., by="talhao") %>% 
-	mutate(ht_est = ifelse(is.na(ht),
-		exp(b0 + b1 * (1/dap) + b2 * log(hd)),
-		ht))
+df3 = merge(df2, dfCoef, by = 'talhao')
+
+df3$HtEst = ifelse(is.na(df3$ht), 
+                   exp(df3$b0 + df3$b1 * (1/df3$dap) + df3$b2 * log(df3$Hd)), 
+                   df3$ht)
+
+#write.csv(df, 'df_processed.csv')
